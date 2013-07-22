7cbe378bac0fa70a7c27d6444765d781d87c99d6
plcl <plcl@64c129e3-8119-4a17-9932-72cb407d3134>
2013-07-22 13:55:38 +0000

    Galician translation updated. Thanks to Miguel Bouzada

---
+++ mobile/vmpkui/i18n/vmpkn9_gl.ts
@@ -0,0 +1,340 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="gl">
+<context>
+    <name>About</name>
+    <message>
+        <location filename="../qml/About.qml" line="24"/>
+        <source>About</source>
+        <translation>Sobre</translation>
+    </message>
+    <message>
+        <location filename="../qml/About.qml" line="61"/>
+        <source>VMPK &amp; FluidSynth</source>
+        <translation>VMPK &amp; FluidSynth</translation>
+    </message>
+    <message>
+        <location filename="../qml/About.qml" line="68"/>
+        <source>v%1</source>
+        <translation>v%1</translation>
+    </message>
+    <message>
+        <location filename="../qml/About.qml" line="103"/>
+        <source>&lt;p&gt;&lt;a href=&apos;http://vmpk.sf.net&apos;&gt;VMPK (Virtual MIDI Piano Keyboard)&lt;/a&gt;.&lt;br&gt;Copyright © 2008-2011, &lt;a href=&apos;mailto:plcl@users.sf.net?subject=VMPK&apos;&gt; Pedro Lopez-Cabanillas &amp;lt;plcl@users.sf.net&amp;gt;&lt;/a&gt;&lt;/p&gt;&lt;p&gt;This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.&lt;/p&gt;&lt;p&gt;This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;  without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details. You should have received a copy of the GNU General Public License  along with this program. If not, see &lt;a href=&apos;http://www.gnu.org/licenses/&apos;&gt;http://www.gnu.org/licenses/&lt;/a&gt;.&lt;/p&gt;&lt;p&gt;The following copyright notice is also applicable:&lt;/p&gt;&lt;p&gt;&lt;a href=&apos;http://www.fluidsynth.org&apos;&gt;FluidSynth&lt;/a&gt;, A Software Synthesizer. Copyright © 2003-2011 Peter Hanappe and others.&lt;/p&gt;</source>
+        <translation>&lt;p&gt;&lt;a href=&apos;http://vmpk.sf.net/index.es.html&apos;&gt;VMPK (Virtual MIDI Piano Keyboard)&lt;/a&gt;.&lt;br&gt;Copyright © 2008-2011, &lt;a href=&apos;mailto:plcl@users.sf.net?subject=VMPK&apos;&gt; Pedro Lopez-Cabanillas &amp;lt;plcl@users.sf.net&amp;gt;&lt;/a&gt;&lt;/p&gt;&lt;p&gt;Este programa é software libre: vostede pode redistribuílo e/ou modificalo baixo os termos da Licenza Pública Xeral de GNU publicada pola Free Software Foundation, xa sexa a versión 3 da licenza, ou (a súa elección) calquera versión posterior.&lt;/p&gt;&lt;p&gt;Este programa distribúese coa esperanza de que sexa útil, mais SEN NINGUNHA GARANTÍA, incluso sen a garantía MERCANTIL implícita ou sen garantir a CONVENIENCIA PARA UN PROPÓSITO PARTICULAR. Vexase a Licenza Pública Xeral de GNU para omter máis detalles.&lt;/p&gt;Debera ter recibido unha copia da Licenza Pública Xeral xunto con este programa. De non ser así, vexa &lt;a href=&apos;http://www.gnu.org/licenses/&apos;&gt;http://www.gnu.org/licenses/&lt;/a&gt;.&lt;/p&gt;&lt;p&gt;O seguinte copyright tamén é aplicábel:&lt;/p&gt;&lt;p&gt;&lt;a href=&apos;http://www.fluidsynth.org&apos;&gt;FluidSynth&lt;/a&gt;, Un sintetizador por software, Copyright © 2003-2011 Peter Hanappe e outros.&lt;/p&gt;</translation>
+    </message>
+</context>
+<context>
+    <name>ColorSelectionDialog</name>
+    <message>
+        <location filename="../qml/ColorSelectionDialog.qml" line="56"/>
+        <source>Color Selection</source>
+        <translation>Selección de cor</translation>
+    </message>
+</context>
+<context>
+    <name>Help</name>
+    <message>
+        <location filename="../qml/Help.qml" line="25"/>
+        <source>Help</source>
+        <translation>Axuda</translation>
+    </message>
+</context>
+<context>
+    <name>MainPage</name>
+    <message>
+        <location filename="../qml/MainPage.qml" line="42"/>
+        <source>Instrument Selection</source>
+        <translation>Selección de instrumentos</translation>
+    </message>
+    <message>
+        <location filename="../qml/MainPage.qml" line="56"/>
+        <source>Channel Selection</source>
+        <translation>Selección de canles</translation>
+    </message>
+    <message>
+        <location filename="../qml/MainPage.qml" line="72"/>
+        <source>Base Octave Selection</source>
+        <translation>Selección de oitava base</translation>
+    </message>
+    <message>
+        <location filename="../qml/MainPage.qml" line="88"/>
+        <source>Controller Selection</source>
+        <translation>Selección de controlador</translation>
+    </message>
+    <message>
+        <location filename="../qml/MainPage.qml" line="110"/>
+        <source>Common</source>
+        <translation>Común</translation>
+    </message>
+    <message>
+        <location filename="../qml/MainPage.qml" line="111"/>
+        <source>Velocity</source>
+        <translation>Velocidade</translation>
+    </message>
+    <message>
+        <location filename="../qml/MainPage.qml" line="112"/>
+        <source>Instrument</source>
+        <translation>Instrumento</translation>
+    </message>
+    <message>
+        <location filename="../qml/MainPage.qml" line="113"/>
+        <source>Controller</source>
+        <translation>Controlador</translation>
+    </message>
+    <message>
+        <location filename="../qml/MainPage.qml" line="114"/>
+        <source>Bender</source>
+        <translation>Inflexión</translation>
+    </message>
+    <message>
+        <location filename="../qml/MainPage.qml" line="137"/>
+        <source>Transpose:</source>
+        <translation>Transposición:</translation>
+    </message>
+    <message>
+        <location filename="../qml/MainPage.qml" line="164"/>
+        <source>Velocity:</source>
+        <translation>Velocidade:</translation>
+    </message>
+    <message>
+        <location filename="../qml/MainPage.qml" line="185"/>
+        <location filename="../qml/MainPage.qml" line="253"/>
+        <location filename="../qml/MainPage.qml" line="299"/>
+        <source>Turn:</source>
+        <translation>Xiro:</translation>
+    </message>
+    <message>
+        <location filename="../qml/MainPage.qml" line="272"/>
+        <source>Bender:</source>
+        <translation>Inflexión:</translation>
+    </message>
+    <message>
+        <location filename="../qml/MainPage.qml" line="392"/>
+        <source>VMPK &amp; FluidSynth v%1</source>
+        <translation>VMPK &amp; FluidSynth v%1</translation>
+    </message>
+    <message>
+        <location filename="../qml/MainPage.qml" line="426"/>
+        <source>Settings</source>
+        <translation>Axustes</translation>
+    </message>
+    <message>
+        <location filename="../qml/MainPage.qml" line="211"/>
+        <source>Panic!</source>
+        <translation>Pánico!</translation>
+    </message>
+    <message>
+        <location filename="../qml/MainPage.qml" line="357"/>
+        <source>Channel %1</source>
+        <translation>Canle %1</translation>
+    </message>
+    <message>
+        <location filename="../qml/MainPage.qml" line="360"/>
+        <source>Octave %1</source>
+        <translation>Oitava %1</translation>
+    </message>
+    <message>
+        <location filename="../qml/MainPage.qml" line="382"/>
+        <source>Volume: %1%</source>
+        <translation>Volume: %1%</translation>
+    </message>
+    <message>
+        <location filename="../qml/MainPage.qml" line="407"/>
+        <source>Show Note Names</source>
+        <translation>Mostrar os nomes das notas</translation>
+    </message>
+    <message>
+        <location filename="../qml/MainPage.qml" line="418"/>
+        <source>Help</source>
+        <translation>Axuda</translation>
+    </message>
+    <message>
+        <location filename="../qml/MainPage.qml" line="434"/>
+        <source>About</source>
+        <translation>Sobre</translation>
+    </message>
+</context>
+<context>
+    <name>PianoKeyboard</name>
+    <message>
+        <location filename="../pianokeyboard.cpp" line="535"/>
+        <location filename="../pianokeyboard.cpp" line="547"/>
+        <source>C</source>
+        <translation>Do</translation>
+    </message>
+    <message utf8="true">
+        <location filename="../pianokeyboard.cpp" line="536"/>
+        <source>C♯</source>
+        <translation>Do♯</translation>
+    </message>
+    <message>
+        <location filename="../pianokeyboard.cpp" line="537"/>
+        <location filename="../pianokeyboard.cpp" line="549"/>
+        <source>D</source>
+        <translation>Re</translation>
+    </message>
+    <message utf8="true">
+        <location filename="../pianokeyboard.cpp" line="538"/>
+        <source>D♯</source>
+        <translation>Re♯</translation>
+    </message>
+    <message>
+        <location filename="../pianokeyboard.cpp" line="539"/>
+        <location filename="../pianokeyboard.cpp" line="551"/>
+        <source>E</source>
+        <translation>Mi</translation>
+    </message>
+    <message>
+        <location filename="../pianokeyboard.cpp" line="540"/>
+        <location filename="../pianokeyboard.cpp" line="552"/>
+        <source>F</source>
+        <translation>Fa</translation>
+    </message>
+    <message utf8="true">
+        <location filename="../pianokeyboard.cpp" line="541"/>
+        <source>F♯</source>
+        <translation>Fa♯</translation>
+    </message>
+    <message>
+        <location filename="../pianokeyboard.cpp" line="542"/>
+        <location filename="../pianokeyboard.cpp" line="554"/>
+        <source>G</source>
+        <translation>Sol</translation>
+    </message>
+    <message utf8="true">
+        <location filename="../pianokeyboard.cpp" line="543"/>
+        <source>G♯</source>
+        <translation>Sol♯</translation>
+    </message>
+    <message>
+        <location filename="../pianokeyboard.cpp" line="544"/>
+        <location filename="../pianokeyboard.cpp" line="556"/>
+        <source>A</source>
+        <translation>La</translation>
+    </message>
+    <message utf8="true">
+        <location filename="../pianokeyboard.cpp" line="545"/>
+        <source>A♯</source>
+        <translation>La♯</translation>
+    </message>
+    <message>
+        <location filename="../pianokeyboard.cpp" line="546"/>
+        <location filename="../pianokeyboard.cpp" line="558"/>
+        <source>B</source>
+        <translation>Si</translation>
+    </message>
+    <message utf8="true">
+        <location filename="../pianokeyboard.cpp" line="548"/>
+        <source>D♭</source>
+        <translation>Re♭</translation>
+    </message>
+    <message utf8="true">
+        <location filename="../pianokeyboard.cpp" line="550"/>
+        <source>E♭</source>
+        <translation>Mi♭</translation>
+    </message>
+    <message utf8="true">
+        <location filename="../pianokeyboard.cpp" line="553"/>
+        <source>G♭</source>
+        <translation>Sol♭</translation>
+    </message>
+    <message utf8="true">
+        <location filename="../pianokeyboard.cpp" line="555"/>
+        <source>A♭</source>
+        <translation>La♭</translation>
+    </message>
+    <message utf8="true">
+        <location filename="../pianokeyboard.cpp" line="557"/>
+        <source>B♭</source>
+        <translation>Si♭</translation>
+    </message>
+</context>
+<context>
+    <name>Preferences</name>
+    <message>
+        <location filename="../qml/Preferences.qml" line="25"/>
+        <source>Settings</source>
+        <translation>Axustes</translation>
+    </message>
+    <message>
+        <location filename="../qml/Preferences.qml" line="40"/>
+        <location filename="../qml/Preferences.qml" line="103"/>
+        <source>Number of Octaves</source>
+        <translation>Número de oitavas</translation>
+    </message>
+    <message>
+        <location filename="../qml/Preferences.qml" line="47"/>
+        <source>Key Pressed Color</source>
+        <translation>Cor das teclas premidas</translation>
+    </message>
+    <message>
+        <location filename="../qml/Preferences.qml" line="55"/>
+        <location filename="../qml/Preferences.qml" line="115"/>
+        <source>SoundFont File</source>
+        <translation>Ficheiro SoundFont</translation>
+    </message>
+    <message>
+        <location filename="../qml/Preferences.qml" line="62"/>
+        <location filename="../qml/Preferences.qml" line="128"/>
+        <source>Language</source>
+        <translation>Idioma</translation>
+    </message>
+    <message>
+        <location filename="../qml/Preferences.qml" line="73"/>
+        <source>Inverted Theme</source>
+        <translation>Contraste invertido</translation>
+    </message>
+    <message>
+        <location filename="../qml/Preferences.qml" line="144"/>
+        <source>Language changed</source>
+        <translation>O idioma cambiou</translation>
+    </message>
+    <message>
+        <location filename="../qml/Preferences.qml" line="145"/>
+        <source>The new language will take effect after restarting the program</source>
+        <translation>A novo idioma será efectivo despois de reiniciar o programa</translation>
+    </message>
+    <message>
+        <location filename="../qml/Preferences.qml" line="146"/>
+        <source>Accept</source>
+        <translation>Aceptar</translation>
+    </message>
+    <message>
+        <location filename="../qml/Preferences.qml" line="151"/>
+        <source>%1 Octaves</source>
+        <translation>%1 oitavas</translation>
+    </message>
+</context>
+<context>
+    <name>Splash</name>
+    <message>
+        <location filename="../qml/Splash.qml" line="70"/>
+        <source>VMPK &amp; FluidSynth</source>
+        <translation>VMPK &amp; FluidSynth</translation>
+    </message>
+</context>
+<context>
+    <name>SynthEngine</name>
+    <message>
+        <location filename="../synthengine.cpp" line="175"/>
+        <source>1:Modulation</source>
+        <translation>1:Modulación</translation>
+    </message>
+    <message>
+        <location filename="../synthengine.cpp" line="176"/>
+        <source>7:Volume</source>
+        <translation>7:Volume</translation>
+    </message>
+    <message>
+        <location filename="../synthengine.cpp" line="177"/>
+        <source>10:Pan</source>
+        <translation>10:Panorama</translation>
+    </message>
+    <message>
+        <location filename="../synthengine.cpp" line="178"/>
+        <source>64:Sustain</source>
+        <translation>64:Sostido</translation>
+    </message>
+</context>
+</TS>
