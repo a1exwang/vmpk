c6df20259075908af4b90e29e7b8c4d1c36bdd38
mezzoforte <mezzoforte@64c129e3-8119-4a17-9932-72cb407d3134>
2014-01-08 19:18:39 +0000

    Proposal for a new version of the virtual piano plugin  - convenient keyboard mapping  - customization improved  - easier integration  - new system for configuring the plugin

--- vpiano/plugin/pianokey.h
+++ vpiano/plugin/pianokey.h
@@ -1,6 +1,7 @@
 /*
-    Virtual Piano Widget for Qt4 
-    Copyright (C) 2008-2013, Pedro Lopez-Cabanillas <plcl@users.sf.net>
+    MIDI Virtual Piano Keyboard
+    Copyright (C) 2008-2014, Pedro Lopez-Cabanillas <plcl@users.sf.net>
+    Copyright (C) 2014,      Davy Triponney         <davy.triponney@gmail.com>
 
     This program is free software; you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
@@ -12,35 +13,47 @@
     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
     GNU General Public License for more details.
 
-    You should have received a copy of the GNU General Public License along 
+    You should have received a copy of the GNU General Public License along
     with this program; If not, see <http://www.gnu.org/licenses/>.
 */
 
 #ifndef PIANOKEY_H_
 #define PIANOKEY_H_
 
-#include "pianodefs.h"
-#include <QtGui/QGraphicsRectItem>
-#include <QtGui/QBrush>
+#include "pianokeybd.h"
+#include <QGraphicsRectItem>
+#include <QBrush>
 
-class VPIANO_EXPORT PianoKey : public QGraphicsRectItem
+class PianoKey : public QGraphicsRectItem
 {
 public:
-    PianoKey(QGraphicsItem * parent = 0 ) 
-        : QGraphicsRectItem(parent), m_pressed(false) { }
-    PianoKey(const QRectF &rect, const QBrush &brush, const int note); 
+    PianoKey(QGraphicsItem * parent = 0 ) : QGraphicsRectItem(parent), m_pressed(false) {}
     PianoKey(const QRectF &rect, const bool black, const int note);
     void paint(QPainter *painter, const QStyleOptionGraphicsItem *, QWidget *);
     int getNote() const { return m_note; }
+    void setBrush(const QBrush& b) { m_brush = b; }
     void setPressedBrush(const QBrush& b) { m_selectedBrush = b; }
+    void resetBrush();
     bool isPressed() const { return m_pressed; }
     void setPressed(bool p);
+    void setMarker(PianoKeybd::MarkerType type);
+    int isBlack() const { return m_black; }
+
+    static QBrush BLACK_BRUSH;
+    static QBrush WHITE_BRUSH;
+
 private:
+    void drawMarker(QPainter *painter, QRectF &rect, PianoKeybd::MarkerType type);
+    static QColor getBorderColor(PianoKeybd::MarkerType type);
+    static QColor getFillColor(PianoKeybd::MarkerType type);
+    void flipPainter(QPainter * painter, QRectF &rect);
+
     bool m_pressed;
     QBrush m_selectedBrush;
     QBrush m_brush;
     int m_note;
     bool m_black;
+    PianoKeybd::MarkerType m_markerType;
 };
 
 #endif /*PIANOKEY_H_*/
--- vpiano/plugin/pianoscene.h
+++ vpiano/plugin/pianoscene.h
@@ -1,6 +1,7 @@
 /*
-    Virtual Piano Widget for Qt4 
-    Copyright (C) 2008-2013, Pedro Lopez-Cabanillas <plcl@users.sf.net>
+    MIDI Virtual Piano Keyboard
+    Copyright (C) 2008-2014, Pedro Lopez-Cabanillas <plcl@users.sf.net>
+    Copyright (C) 2014,      Davy Triponney         <davy.triponney@gmail.com>
 
     This program is free software; you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
@@ -12,109 +13,132 @@
     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
     GNU General Public License for more details.
 
-    You should have received a copy of the GNU General Public License along 
+    You should have received a copy of the GNU General Public License along
     with this program; If not, see <http://www.gnu.org/licenses/>.
 */
 
 #ifndef PIANOSCENE_H_
 #define PIANOSCENE_H_
 
+#include "pianokeybd.h"
 #include "pianokey.h"
 #include "keylabel.h"
 #include "keyboardmap.h"
-#include <QtGui/QGraphicsScene>
-#include <QtCore/QHash>
+#include <QGraphicsScene>
+#include <QHash>
 
-class PianoHandler
-{
-public:
-    virtual ~PianoHandler() {}
-    virtual void noteOn( const int note ) = 0;
-    virtual void noteOff( const int note ) = 0;
-};
 
-class VPIANO_EXPORT PianoScene : public QGraphicsScene
+class PianoScene : public QGraphicsScene
 {
     Q_OBJECT
     
 public:
-    PianoScene ( const int baseOctave, 
-                 const int numOctaves,
-                 const QColor& keyPressedColor = QColor(),
-                 QObject * parent = 0 );
+    PianoScene (const int startKey, const int numKeys, PianoScene * previousScene = NULL, QObject * parent = NULL);
     
     QSize sizeHint() const;
-    void setKeyboardMap( KeyboardMap* map ) { m_keybdMap = map; }
-    KeyboardMap* getKeyboardMap() const { return m_keybdMap; }
-    PianoHandler* getPianoHandler() const { return m_handler; }
-    void setPianoHandler(PianoHandler* handler) { m_handler = handler; }
-    QColor getKeyPressedColor() const { return m_keyPressedColor; }
-    void setKeyPressedColor(const QColor& color);
-    int getMinNote() const { return m_minNote; } 
-    void setMinNote(const int note);
-    int getMaxNote() const { return m_maxNote; }
-    void setMaxNote(const int note);
+    static void initKeyboardMap( KeyboardMap* map ) { m_keybdMap = map; }
+
+    // Color management
+    void setColorationType(PianoKeybd::ColorationType colorationType);
+    PianoKeybd::ColorationType getColorationType() { return m_colorationType; }
+    void setColor(int num, QColor color);
+    QColor getColor(int num) { return m_palette.value(num); }
+
+    // Customization
+    void addCustomColor(int key, QColor color);
+    void addMarker(int key, PianoKeybd::MarkerType markerType);
+    void clearCustomization();
+    void resetCustomization(int key, PianoKeybd::CustomizationType type);
+
+    // Labels
+    void setLabelType(PianoKeybd::LabelType type);
+    PianoKeybd::LabelType getLabelType() { return m_labelType; }
+    void setMiddleC(PianoKeybd::MiddleKey middleC);
+    PianoKeybd::MiddleKey getMiddleC() { return m_middleC; }
+    void setCustomNoteNames(const QStringList& names);
+    QStringList customNoteNames() const { return m_names_custom; }
+    void setIndicesInLabels(bool enabled);
+    bool indicesInLabels() { return m_indicesInLabels; }
+
     int getTranspose() const { return m_transpose; }
     void setTranspose(const int transpose);
-    bool showLabels() const { return m_showLabels; }
-    void setShowLabels(const bool show);
-    bool useFlats() const { return m_useFlats; }
-    void setUseFlats(const bool use);
-
-    void showNoteOn( const int note, int vel = 0 );
-    void showNoteOff( const int note, int vel = 0 );
-    int baseOctave() const { return m_baseOctave; }
-    void setBaseOctave( const int base );
-    int numOctaves() const { return m_numOctaves; }
+    bool isKeyboardEnabled() const { return m_keyboardEnabled; }
+    void setKeyboardEnabled( const bool enable );
+    bool isMouseEnabled() const { return m_mouseEnabled; }
+    void setMouseEnabled( const bool enable );
+    bool isTouchEnabled() const { return m_touchEnabled; }
+    void setTouchEnabled( const bool enable );
+    void showNoteOn(const int note, int vel = -1, int channel = -1);
+    void showNoteOff(const int note, int channel = -1);
+    int numKeys() const { return m_numKeys; }
+    int startKey() const { return m_startKey; }
     void allKeysOff();
     void keyOn( const int note );
     void keyOff( const int note );
     bool getRawKeyboardMode() const { return m_rawkbd; }
     void setRawKeyboardMode(const bool b);
-    void useCustomNoteNames(const QStringList& names);
-    void useStandardNoteNames();
     int getVelocity() { return m_velocity; }
     void setVelocity(const int velocity) { m_velocity = velocity; }
+    int getChannel() const { return m_channel; }
+    void setChannel(const int channel) { m_channel = channel; }
+
+    void refreshLabels();
 
 signals:
-    void noteOn(int n);
+    void noteOn(int n, int v);
     void noteOff(int n);
+    void mouseOver(int n);
 
 protected:
-    void showKeyOn( PianoKey* key, int vel );
-    void showKeyOff( PianoKey* key, int vel );
-    void keyOn( PianoKey* key );
-    void keyOff( PianoKey* key );
-    PianoKey* getKeyForPos( const QPointF& p ) const;
-    PianoKey* getPianoKey( const int key ) const;
-    QString noteName(const int note);
-
-    void mouseMoveEvent ( QGraphicsSceneMouseEvent * mouseEvent );
-    void mousePressEvent ( QGraphicsSceneMouseEvent * mouseEvent );
-    void mouseReleaseEvent ( QGraphicsSceneMouseEvent * mouseEvent );
-    void keyPressEvent ( QKeyEvent * keyEvent );
-    void keyReleaseEvent ( QKeyEvent * keyEvent );
+    void showKeyOn(PianoKey* key, int vel = -1, int channel = -1);
+    void showKeyOff( PianoKey* key );
+    void keyOn(PianoKey* key, qreal pressure = -1);
+    void keyOff(PianoKey* key);
+    PianoKey* getKeyForPos(const QPointF& p) const;
+    QString noteName(int note);
+    void mouseMoveEvent(QGraphicsSceneMouseEvent * mouseEvent);
+    void mousePressEvent(QGraphicsSceneMouseEvent * mouseEvent);
+    void mouseReleaseEvent(QGraphicsSceneMouseEvent * mouseEvent);
+    void keyPressEvent(QKeyEvent * keyEvent );
+    void keyReleaseEvent(QKeyEvent * keyEvent );
+    bool event(QEvent *event);
 
 private:
-    void hideOrShowKeys();
-    void refreshLabels();
-    
-    int m_baseOctave;
-    int m_numOctaves;
+    void initConfiguration(PianoScene *previousScene);
+    void initLabels();
+    void createKeyboard();
+    void refreshKeys();
+    void triggerNoteOn(const int note, const int vel);
+    void triggerNoteOff(const int note);
+    static qreal sceneWidth(int startKey, int keys);
+    static float getOffsetX(int numKey);
+    static QString formatSequence(QKeySequence sequence);
+
+    int m_numKeys;
+    int m_startKey;
     int m_minNote;
     int m_maxNote;
     int m_transpose;
-    bool m_showLabels;
-    bool m_useFlats;
     bool m_rawkbd;
-    QColor m_keyPressedColor;
+    bool m_keyboardEnabled;
+    bool m_mouseEnabled;
+    bool m_touchEnabled;
     bool m_mousePressed;
     int m_velocity;
-    PianoHandler* m_handler;
-    KeyboardMap* m_keybdMap;
-    QList<PianoKey*> m_keys;
-    QList<KeyLabel*> m_labels;
-    QStringList m_noteNames;
+    int m_channel;
+    PianoKeybd::LabelType m_labelType;
+    PianoKeybd::MiddleKey m_middleC;
+    PianoKeybd::ColorationType m_colorationType;
+    QHash<int, PianoKey*> m_keys;
+    QMap<int, KeyLabel*> m_labels;
+    QStringList m_names_s, m_names_f, m_names_custom;
+    bool m_indicesInLabels;
+    QMap<int, QColor> m_palette;
+    QMap<int, QColor> m_customColors;
+    QMap<int, PianoKeybd::MarkerType> m_markers;
+
+    static int MIN_NOTE, MAX_NOTE, KEYWIDTH, KEYHEIGHT;
+    static KeyboardMap* m_keybdMap;
 };
 
 #endif /*PIANOSCENE_H_*/
